6dcd13f12fd6ec5900d33952b0221ab18a7e8725
AnckerStudios <horohi26@gmail.com>
2022-10-24 17:47:49 +0400

    train

--- src/element/Train.js
+++ src/element/Train.js
@@ -1,37 +1,31 @@
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 import { useFrame } from "@react-three/fiber";
-import { Vector3 } from "three";
+import { Vector2, Vector3 } from "three";
 
 function Train({pos = [0,0,0], mtrx=[]}) {
   const { nodes, materials } = useGLTF("/train.glb");
   const ref = useRef();
+  let speed = 0.05;
+  let startCell = {x:0,y:0};
   let startPos = new Vector3(0,0,0);
-  let curPos = new Vector3(0,0,0);
+  let curPos = new Vector2(0,0);
   let curCell = new Vector3(0,0,0);
   let targetPos = new Vector3(0,0,0);
-  let targetCell = new Vector3(0,0,0);
-  let direction = [true,true]
-  let dis= 0;
+  let targetCell = {x:0,y:0};
+  let dir = {x:0,y:0}
+  let iteration = 0;
+  let isStopped = false;
   useFrame((state, delta) => {
-    //console.log(ref.current.position.x)
-    if(curPos.x === targetPos.x && curPos.y === targetPos.y){
-      console.log('fff')
-      startPos = targetPos;
-      targetCell = new Vector3(targetCell.x+1,0);
-      targetPos = targetCell;
-      dis = 0;
+    if(!isStopped){
+      if(iteration === 0){
+
+      }else{
+        
+      }
+      ref.current.position.x += speed * dir.x;
+      ref.current.position.y += speed * dir.y;
     }
-    console.log(curPos)
-    // let a = ref.current.position;
-    // let b = getVector(curVector.x);
-    curPos = startPos.lerp(targetPos,dis);
-    curPos.x = +curPos.x.toFixed(2)
-    dis += 0.01;
-    dis = +dis.toFixed(2);
-    ref.current.position.x = curPos.x;
-    ref.current.position.y = curPos.y;
-    
     })
 
 //   function getVector(x,y){
@@ -39,34 +33,42 @@ function Train({pos = [0,0,0], mtrx=[]}) {
 //     console.log(vector);
 //     return vector;
 //   }
-//   function getNext(x,y){
-//     switch(mtrx[x][y].state){
-//       case 'x':
-//         return [x+(direction[0] ? 1 : -1),0]
-//       case 'y':
-//         return [0,y+(direction[1] ? 1 : -1)]
-//       case 'dx':
+  function getNextCell(x,y){
+    let newX = x+(direction[0] ? 1 : -1);
+    let newY = y+(direction[1] ? 1 : -1);
+
+    if(newX < 0 || newX > mtrx.length || newY < 0 || newY > mtrx[0].length){
+      isStopped = true;
+      return mtrx[x][y];
+    }
+    return mtrx[newX][y];
+    switch(mtrx[x][y].state){
+      case 'x':
+        return [x+(direction[0] ? 1 : -1),0]
+      case 'y':
+        return [0,y+(direction[1] ? 1 : -1)]
+      case 'dx':
          
-//       case 'dy':
+      case 'dy':
           
-//       case 'rx_top':
+      case 'rx_top':
          
-//       case 'rx_down':
+      case 'rx_down':
           
-//       case 'rx_left':
+      case 'rx_left':
           
-//       case 'rx_right':
+      case 'rx_right':
           
-//       case 'ry_top':
+      case 'ry_top':
           
-//       case 'ry_down':
+      case 'ry_down':
           
-//       case 'ry_left':
+      case 'ry_left':
           
-//       case 'ry_right':
-//           break;
-//   }
-// }
+      case 'ry_right':
+          break;
+  }
+}
   return (
     <group dispose={null}>
       <mesh
