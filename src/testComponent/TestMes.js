cb3ad58267bd4b0512007df814615bbb02c3569b
AnckerStudios <horohi26@gmail.com>
2022-10-27 21:25:05 +0400

    message test 0.2

--- src/testComponent/TestMes.js
+++ src/testComponent/TestMes.js
@@ -3,10 +3,21 @@ import React, { useEffect, useRef, useState } from "react";
 
 
 function TestMes() {
+    const [fieldMtrx, setFieldMtrx] = useState(createMtrx(objSettings.x,objSettings.y));
+    function createMtrx(x, y){
+        let arr = [];
+        for(let i = 0; i < x; i++){
+          arr.push([])
+          for(let j = 0; j < y; j++){
+            arr[i].push({id: y*i+j, x: i, y: j, state: {}})
+          }
+        }
+        return arr
+      }
     function sendMes(){
         axios.post('http://localhost:8080/api/topology', {
             title: "Hi",
-            body: "Ky"
+            body: fieldMtrx
           })
         .then(response => {
             console.log("response", response.data);
@@ -18,7 +29,7 @@ function TestMes() {
     return (
         <div className="flex justify-center ">
             <button className="border rounded bg-yellow-300 px-5 py-2 text-lg font-bold m-5 hover:bg-orange-500" onClick={()=>sendMes()}>Общение</button>
-
+            
         </div>
     );
 }
