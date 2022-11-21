ad24d63e97c6d542c602afc400f14ca85a14224e
AnckerStudios <horohi26@gmail.com>
2022-11-21 21:24:13 +0400

    arteom message 0.3.2

--- src/element/Preview.js
+++ src/element/Preview.js
@@ -27,39 +27,51 @@ function Preview({mtrx = [], objSet = {x: 16, y:16}, prev = true, err=false}) {
                 for(let state in item.state){
                     switch(state){
                         case 'x':
+                            if(item.state.x === true)
                             changesL.push({x:item.x, y:item.y, rot: {x: 0, y: 0, z: 0}, index: ((item.x * row.length + item.y) * (prev ? 1 : 2))});
                             break;
                         case 'y':
+                            if(item.state.y === true)
                             changesL.push({x:item.x, y:item.y, rot: {x: 0, y: 0, z: Math.PI/2}, index: ((item.x * row.length + item.y) * (prev ? 1 : 2) + (prev ? 0 : 1))});
                             break;
                         case 'dx':
+                            if(item.state.dx === true)
                             changesD.push({x:item.x, y:item.y, rot: {x: 0, y: 0, z: 0}, index: ((item.x * row.length + item.y) * (prev ? 1 : 2))});
                             break;
                         case 'dy':
+                            if(item.state.dy === true)
                             changesD.push({x:item.x, y:item.y, rot: {x: 0, y: 0, z: Math.PI/2}, index: ((item.x * row.length + item.y) * (prev ? 1 : 2) + (prev ? 0 : 1))});
                             break;
                         case 'rx_top':
+                            if(item.state.rx_top === true)
                             changesR.push({x:item.x, y:item.y, rot: {x: 0, y: 0, z: Math.PI}, index: ((item.x * row.length + item.y) * (prev ? 1 : 8))});
                             break;
                         case 'rx_down':
+                            if(item.state.rx_down === true)
                             changesR.push({x:item.x, y:item.y, rot: {x: 0, y: 0, z: 0}, index: ((item.x * row.length + item.y) * (prev ? 1 : 8) + (prev ? 0 : 1))});
                             break;
                         case 'rx_left':
+                            if(item.state.rx_left === true)
                             changesR.push({x:item.x, y:item.y, rot: {x: Math.PI, y: 0, z: Math.PI}, index: ((item.x * row.length + item.y) * (prev ? 1 : 8) + (prev ? 0 : 2))});
                             break;
                         case 'rx_right':
+                            if(item.state.rx_right === true)
                             changesR.push({x:item.x, y:item.y, rot: {x: Math.PI, y: 0, z: 0}, index: ((item.x * row.length + item.y) * (prev ? 1 : 8) + (prev ? 0 : 3))});
                             break;
                         case 'ry_top':
+                            if(item.state.ry_top === true)
                             changesR.push({x:item.x, y:item.y, rot: {x: 0, y: Math.PI, z: -Math.PI/2}, index: ((item.x * row.length + item.y) * (prev ? 1 : 8) + (prev ? 0 : 4))});
                             break;
                         case 'ry_down':
+                            if(item.state.ry_down === true)
                             changesR.push({x:item.x, y:item.y, rot: {x: 0, y: Math.PI, z: Math.PI/2}, index: ((item.x * row.length + item.y) * (prev ? 1 : 8) + (prev ? 0 : 5))});
                             break;
                         case 'ry_left':
+                            if(item.state.ry_left === true)
                             changesR.push({x:item.x, y:item.y, rot: {x: 0, y: 0, z: -Math.PI/2}, index: ((item.x * row.length + item.y) * (prev ? 1 : 8) + (prev ? 0 : 6))});
                             break;
                         case 'ry_right':
+                            if(item.state.ry_right === true)
                             changesR.push({x:item.x, y:item.y, rot: {x: 0, y: 0, z: Math.PI/2}, index: ((item.x * row.length + item.y) * (prev ? 1 : 8) + (prev ? 0 : 7))});
                             break;
                         case 'plate':
