0c87b9f80ce3d0579de601bdddcd97fc21d0eca7
AnckerStudios <horohi26@gmail.com>
2022-12-19 23:44:36 +0400

    Schedule Editor debug

--- src/components/scheduleComponents/AddEntry.js
+++ src/components/scheduleComponents/AddEntry.js
@@ -12,53 +12,87 @@ function AddEntry({schedule, setSchedule, id}) {
             }
         })
         .then(function (response) {
-            setPlatforms(response.data.plates);
-            setInOut(response.data.inOut);
+            let resPlates = response.data.plates;
+            let resInOut = response.data.inOut;
+            setPlatforms(resPlates);
+            setInOut(resInOut);
+            setEntry({
+                plate: resPlates[0], 
+                plateLine: resPlates[0].lines[0],  //GGGGGGGGGGGGGGG
+                trainName: "", 
+                arrivalTime: '00:00', 
+                departureTime:'00:00',
+                departureCity:"",
+                arrivalCity:"",
+                typeTrain: type[0], 
+                in:resInOut[0], 
+                out:resInOut[0]});
             console.log(response);
             
         })
         .catch(function (error) {
             console.log(error);
-            setPlatforms([{number: 7, lines:[{x:0,y:0,number:1}]},{number: 8, lines:[{x:0,y:0,number:3},{x:0,y:0,number:4}]},{number: 4, lines:[{x:0,y:0,number:5},{x:0,y:0,number:6}]}]);
-            setInOut([{x:0,y:0,dir:0},{x:15,y:9,dir:0},{x:0,y:3,dir:0},{x:4,y:0,dir:0}]);
+            let resPlates =[{number: 7, dir: true, lines:[{x:0,y:0,number:1}]},{number: 8, dir: true, lines:[{x:0,y:0,number:3},{x:0,y:0,number:4}]},{number: 4, dir: true, lines:[{x:0,y:0,number:5},{x:0,y:0,number:6}]}];
+            let resInOut = [{x:0,y:0,dir:0},{x:15,y:9,dir:0},{x:0,y:3,dir:0},{x:4,y:0,dir:0}];
+            setPlatforms(resPlates);
+            setInOut(resInOut);
+            setEntry({
+                plate: resPlates[0], 
+                plateLine: resPlates[0].lines[0],  //GGGGGGGGGGGGGGG
+                trainName: "", 
+                arrivalTime: '00:00', 
+                departureTime:'00:00',
+                departureCity:"",
+                arrivalCity:"",
+                typeTrain: type[0], 
+                in:resInOut[0], 
+                out:resInOut[0]});
         });
     },[])
-
     const [platforms,setPlatforms] = useState();
     const [inOut,setInOut] = useState();
+
+
     const type = ['Пассаж.','Груз.','Электричка'];
-    const [entry, setEntry] = useState({plate: {number: 1,dir:true, lines:[{x:0,y:0,number:1}]}, plateLine: {x:0,y:0,number:1}, trainName: "gg", arrivalTime: '10:23', departureTime:'12:34',departureCity:"Samara" ,arrivalCity:"Moscow" ,typeTrain: "type", in:{x:0,y:0,dir:2}, out:{x:0,y:0,dir:2}});
+    const [entry, setEntry] = useState({plate: {lines:[]}});
 
     const sity = [{city:"Самара"}, {city:"Москва"},{city:"Санкт-Петербург"},{city:"Оренбург"}, ]
     const sitys = []
     for(let i = 0; i < sity.length; i++){
-         sitys.push(<option>{sity[i].city}</option>)  
-    }
-    const types = []
-    for(let i = 0; i < type.length; i++){
-         types.push(<option>{type[i]}</option>)  
+         sitys.push(<option key={i}>{sity[i].city}</option>)  
     }
 
     function Add(){
         console.log(entry)
         setSchedule([...schedule,entry]);
     }
+    // const [selectLine,setSelectLine]=useState(0);
+    // useEffect(()=>{
+    //     if(selectLine < entry.plate.lines.length){
+    //         setEntry({...entry, plateLine: entry.plate.lines[selectLine]})}
+    //     },[selectLine]);
+    const [arTime, setArTime] = useState({h:0,m:0});
+    const [deTime, setDeTime] = useState({h:0,m:0});
+
+    useEffect(()=>{setEntry({...entry, arrivalTime: `${arTime.h}:${arTime.m}`})},[arTime])
+    useEffect(()=>{setEntry({...entry, departureTime: `${deTime.h}:${deTime.m}`})},[deTime])
+    
     return (
         <div className='formsep'>
             <fieldset>
                 <legend>Добавить запись</legend>
                 <label id = "firstlab">Выберите платформу
-                    <select className = "addsep" value={entry.plate.number} onChange={(e)=>setEntry({...entry, plate: platforms.find(item => item.number == e.target.value), plateLine: entry.plate.lines[0]})}>
-                        {platforms?.map(platform => {
-                            return <option value={platform.number}>Платформа №{platform.number}</option>
+                    <select className = "addsep" onChange={(e)=>setEntry({...entry, plate: platforms[e.target.value]})}>
+                        {platforms?.map((platform,index) => {
+                            return <option key={index} value={index}>Платформа №{platform.number}</option>
                         })}
                     </select>
                 </label>
                 <br/>
                 <label>Выберите путь
-                    <select className = "addsep" value={entry.plateLine.number} onChange={(e)=>setEntry({...entry, plateLine: entry.plate.lines.find(item => item.number == e.target.value)})}>
-                        {entry.plate.lines?.map(line => {
-                            return <option value={line.number}>Путь №{line.number}</option>
+                    <select className = "addsep" onChange={(e)=>setEntry({...entry, plateLine: entry.plate.lines[e.target.value]})}>
+                        {entry.plate.lines?.map((line,index) => {
+                            return <option key={index} value={index}>Путь №{line.number}</option>
                         })}
                     </select>
                 </label>
@@ -69,22 +103,22 @@ function AddEntry({schedule, setSchedule, id}) {
                 <br/>
                 <label>Время прибытия
                     <span id = "tpr">
-                        <input type="number" min="0" max="23" /*onChange={(e)=>setEntry({...entry, arrivalTime: {...entry.arrivalTime,h:e.target.value}})}*//>
+                        <input type="number" min="0" max="23" onChange={(e)=>setArTime({...arTime, h: +e.target.value})}/>
 
-                    <text>:</text>
+                    :
 
-                        <input type="number" min="0" max="59" /*onChange={(e)=>setEntry({...entry, arrivalTime: {...entry.arrivalTime,m:e.target.value}})}*//>
+                        <input type="number" min="0" max="59" onChange={(e)=>setArTime({...arTime, m: +e.target.value})}/>
  
                     </span>
                 </label> <br/>
                 <label>Время отравления
                 <span id='totpr'>
 
-                        <input type="number" min="0" max="23" /*onChange={(e)=>setEntry({...entry, departureTime: {...entry.departureTime,h:e.target.value}})}*//>
+                        <input type="number" min="0" max="23" onChange={(e)=>setDeTime({...deTime, h: +e.target.value})}/>
 
-                    <text>:</text>
+                    :
  
-                        <input type="number" min="0" max="59" /*onChange={(e)=>setEntry({...entry, departureTime: {...entry.departureTime,m:e.target.value}})}*//>
+                        <input type="number" min="0" max="59" onChange={(e)=>setDeTime({...deTime, m: +e.target.value})}/>
 
                     </span>
                 </label>
@@ -101,22 +135,22 @@ function AddEntry({schedule, setSchedule, id}) {
                     </datalist>
                 </label><br/>
                 <label>In
-                    <select className = "addsep" value={entry.in} >
-                        {inOut?.map(io => {
-                            return <option value={{x:io.x,y:io.y,dir:io.dir}}>x: {io.x} y: {io.y}</option>
+                    <select className = "addsep" onChange={(e)=>setEntry({...entry, in: inOut[e.target.value]})}>
+                        {inOut?.map((io,index) => {
+                            return <option  key={index} value={index}>x: {io.x} y: {io.y}</option>
                         })}
                     </select>
                 </label><br/>
                 <label>Out
-                    <select className = "addsep" value={entry.out} >
-                        {inOut?.map(io => {
-                            return <option value={{x:io.x,y:io.y,dir:io.dir}}>x: {io.x} y: {io.y}</option>
+                    <select className = "addsep" onChange={(e)=>setEntry({...entry, out: inOut[e.target.value]})}>
+                        {inOut?.map((io,index) => {
+                            return <option key={index} value={index}>x: {io.x} y: {io.y}</option>
                         })}
                     </select>
                 </label><br/>
-                <label>Выберите тип поезда<select className = "addsep" value={entry.typeTrain} onChange={(e)=>setEntry({...entry, typeTrain: e.target.value})}>
-                        {types?.map(io => {
-                            return <option value={1}>{io}</option>
+                <label>Выберите тип поезда<select className = "addsep" onChange={(e)=>setEntry({...entry, typeTrain: type[e.target.value]})}>
+                        {type?.map((io,index) => {
+                            return <option key={index} value={index}>{io}</option>
                         })}
                     </select></label><br/>
                 <button id = "bb" className='cent' onClick={()=>Add()}>Добавить</button>
--- src/components/scheduleComponents/ScheduleTable.js
+++ src/components/scheduleComponents/ScheduleTable.js
@@ -7,12 +7,12 @@ function ScheduleTable({schedule}) {
 
     const items = []
     for(let i = 0; i < schedule.length; i++){
-        items.push( <tr>
+        items.push( <tr key={i}>
             <td>{schedule[i].plate.number}</td>
             <td>{schedule[i].plateLine.number}</td>
             <td>{schedule[i].trainName}</td>
-            <td>21:05</td>
-            <td>21:05</td>
+            <td>{schedule[i].arrivalTime}</td>
+            <td>{schedule[i].departureTime}</td>
             <td>{schedule[i].departureCity}-{schedule[i].arrivalCity}</td>
             <td>{schedule[i].typeTrain}</td>
         </tr>)  
