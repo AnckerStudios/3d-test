00eb90f1cdcacdcb64f44e28723d30b037e96f0f
AnckerStudios <horohi26@gmail.com>
2022-12-19 20:58:23 +0400

    aaa x2

--- src/components/scheduleComponents/ModalDateSelector.js
+++ src/components/scheduleComponents/ModalDateSelector.js
@@ -8,7 +8,7 @@ function ModalDateSelector({set}) {
     return (
         <div className=" w-96 h-96 bg-slate-300 rounded-xl p-8 flex flex-col gap-4 justify-center items-center">
             Ты лох, ха-ха
-           <input className = " rounded-lg text-center"  type = "text" minLength = "8" maxLength = "8" onChange={(e)=>setDate(e.target.value)}/> 
+           <input className = " rounded-lg text-center"  type = "text" minLength = "10" maxLength = "10" onChange={(e)=>setDate(e.target.value)}/> 
            <button className=" bg-slate-200 hover:bg-fuchsia-300 w-24 p-2 rounded-xl shadow-xl" onClick={()=>set(date)}>ok</button>
         </div>
         
