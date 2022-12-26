6856fc5208082e3ef4ae9bffd1b39e36a84e6e8d
AnckerStudios <horohi26@gmail.com>
2022-12-26 22:14:58 +0400

    city count

--- src/components/cityComponents/CityListItem.js
+++ src/components/cityComponents/CityListItem.js
@@ -8,7 +8,7 @@ function CityListItem({city}) {
         <div className="aspect-square w-3/12 flex relative ">
             <Link className=" bg-slate-300 rounded-xl w-full h-full flex justify-center font-bold items-center hover:bg-orange-300 snap-start " to={`/city/${city.cityName}`}>{city.cityName}</Link>
             <div className=" absolute bottom-1 right-3 flex items-center gap-3 ">
-                    <div>{`${city?.countTopology} топологий`}</div>
+                    <div>{`${city?.count} топологий`}</div>
                     <div className=" h-3 w-3 rounded-full bg-red-600 shadow-xl "/>
             </div>
         </div>
