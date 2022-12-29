e8a1f19eb74bafe14e720aa632e951c20be0d9d8
AnckerStudios <horohi26@gmail.com>
2022-12-30 02:34:08 +0400

    work it 5

--- src/components/scheduleComponents/AddEntry.js
+++ src/components/scheduleComponents/AddEntry.js
@@ -235,7 +235,7 @@ function AddEntry({ inOut, platforms, trains, id, addSchedule, active, setActive
                             </label>
                             <br />
                             <label>Выберите поезд
-                                <select className="addsep" onChange={(e) => { setEntry({ ...entry, train: trains[e.target.value].nameTrain }) }}>
+                                <select className="addsep" onChange={(e) => { setEntry({ ...entry, train: trains[e.target.value] }) }}>
                                     {trains?.map((pl, index) => {
                                         return <option key={index} value={index}>{`№${pl.nameTrain} ${pl.typeTrain}`}</option>
                                     })}
